f6613e9b7a0227bbd1f17699f0ad591d0ad3c7b6
Cesar Gomez <zootechdrum@gmail.com>
2019-12-09 22:04:44 -0800

    added schema to bamazon file

---
+++ bamazon.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE DATABASE bamazon_DB;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(45) NOT NULL,
+    price INT default 0,
+    stock_quantity INT default 0,
+    PRIMARY KEY (item_id)
+);
