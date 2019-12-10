4575417571bbb0172e0e22809ad25d093b27136b
Cesar Gomez <zootechdrum@gmail.com>
2019-12-09 23:09:19 -0800

    Successfully connected to DB added function to verify we are connected

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,22 @@
 var mysql  = require('mysql');
-var prompt = require('prompt');
+var inquirer = require('inquirer');
+
+//Required to connect to our database on Local server
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "bamazon_DB"
+  });
+
+  connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Database is connected!!");
+});
