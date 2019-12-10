97f8236e518dee3493a5178f6e4f4f2e1d716db7
Cesar Gomez <zootechdrum@gmail.com>
2019-12-09 22:51:04 -0800

    renamed file to schema and added seed file for sql database

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+-- Used to seed database table with data
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("DW Drum Set", "Music", 500, 5),
+("Supreme", "Clothing", 400, 20), 
+("Fender Stratocaster", "Music", 300, 45),
+("Tickle Me Elmo", "Toys", 25, 30),
+("Barbie Doll", "Toys", 35, 45);
+
