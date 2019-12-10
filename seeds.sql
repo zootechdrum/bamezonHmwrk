721792a3fdb2e4d0443bc914a0353fcdbcfd6313
Cesar Gomez <zootechdrum@gmail.com>
2019-12-09 22:56:25 -0800

    added more info to the seed.sql file

--- seeds.sql
+++ seeds.sql
@@ -6,5 +6,10 @@ VALUES
 ("Supreme", "Clothing", 400, 20), 
 ("Fender Stratocaster", "Music", 300, 45),
 ("Tickle Me Elmo", "Toys", 25, 30),
+("Nerf Blaster", "Toys", 50, 30),
+"Baby Einstein Doll", "Toys", 10, 90),
+("T-Rex", "Toys", 20, 30),
+("Tama Drum Set", "Music", 600, 30),
+("Fender Ultra Guitar", "Music", 600, 30),
 ("Barbie Doll", "Toys", 35, 45);
 
