37b4e7c71cccf64960e74c78c97b5670f4b1b152
Cesar Gomez <zootechdrum@gmail.com>
2019-12-10 17:59:35 -0800

    Display 2 prompt messages to the terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -34,6 +34,7 @@ var connection = mysql.createConnection({
 
 var query = "SELECT * FROM products"
 
+function showTable() {
 connection.query(query, function(err, res){
   if (err) throw err;
 
@@ -48,5 +49,75 @@ connection.query(query, function(err, res){
     )
   }
   console.log(table.toString())
+  runSearch()
 })
+}
+
+
+function runSearch() {
+  inquirer
+    .prompt([
+      {
+      name: "id",
+      type: "input",
+      message: "What is the item_ID of the item you would like to purchase?",
+      },
+      {
+        name: "id",
+        type: "input",
+        message: "How much would you like to buy ?",
+      }
+    ])
+
+  }
+    // function rangeSearch() {
+    //   inquirer
+    //     .prompt([
+    //       {
+    //         name: "start",
+    //         type: "input",
+    //         message: "Enter starting position: ",
+    //         validate: function(value) {
+    //           if (isNaN(value) === false) {
+    //             return true;
+    //           }
+    //           return false;
+    //         }
+    //       },
+    //       {
+    //         name: "end",
+    //         type: "input",
+    //         message: "Enter ending position: ",
+    //         validate: function(value) {
+    //           if (isNaN(value) === false) {
+    //             return true;
+    //           }
+    //           return false;
+    //         }
+    //       }
+    //     ])
+    //     .then(function(answer) {
+    //       var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+    //       connection.query(query, [answer.start, answer.end], function(err, res) {
+    //         if (err) throw err;
+    //         for (var i = 0; i < res.length; i++) {
+    //           console.log(
+    //             "Position: " +
+    //               res[i].position +
+    //               " || Song: " +
+    //               res[i].song +
+    //               " || Artist: " +
+    //               res[i].artist +
+    //               " || Year: " +
+    //               res[i].year
+    //           );
+    //         }
+    //         runSearch();
+    //       });
+    //     });
+    // }
+
+
+
+showTable();
 
